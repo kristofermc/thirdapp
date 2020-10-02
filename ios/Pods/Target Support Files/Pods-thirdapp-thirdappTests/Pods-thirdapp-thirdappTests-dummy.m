9a6823f044a6dfd7e304a260e0cab4a4874affb5
kristofermc <kris.jussi@gmail.com>
2020-10-02 18:55:10 +0300

    Simple React Native Application for studies

---
+++ ios/Pods/Target Support Files/Pods-thirdapp-thirdappTests/Pods-thirdapp-thirdappTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_thirdapp_thirdappTests : NSObject
+@end
+@implementation PodsDummy_Pods_thirdapp_thirdappTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-thirdapp/Pods-thirdapp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_thirdapp : NSObject
+@end
+@implementation PodsDummy_Pods_thirdapp
+@end
